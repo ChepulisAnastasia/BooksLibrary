0da29ede17d527fd8bf47d50bef7dab870a3481d
ChepulisAnastasia <nastena.chepulis@gmail.com>
2016-11-30 13:13:02 +0300

    Add project files.

---
+++ BooksLibrary/Controllers/BooksController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using BooksLibrary.Models;
+
+namespace BooksLibrary.Controllers
+{
+    [Authorize]
+    [Filters.ExceptionFilter]
+    public class BooksController : Controller
+    {
+        private LibraryDatabaseEntities db = new LibraryDatabaseEntities();
+
+        // GET: Books
+        public ActionResult Index()
+        {
+            return View(db.Books.ToList());
+        }
+
+        // GET: Books/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                throw new Exception("400");
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                throw new Exception("404");
+            }
+            return View(book);
+        }
+
+        // GET: Books/Create
+        [Authorize(Roles = "Manager")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Books/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Manager")]
+        public ActionResult Create([Bind(Include = "ISBN,author,name,publication_type,tome,compiler,language,translator,place_publication,publishing,year,number,price")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        [Authorize(Roles = "Manager")]
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                throw new Exception("400");
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                throw new Exception("404");
+            }
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Manager")]
+        public ActionResult Edit([Bind(Include = "ISBN,author,name,publication_type,tome,compiler,language,translator,place_publication,publishing,year,number,price")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Manager")]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            try
+            {
+                Book book = db.Books.Find(id);
+                db.Books.Remove(book);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                throw new Exception("403");
+            }
+            
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BooksLibrary/Scripts/BooksFilter.js
@@ -0,0 +1,48 @@
+﻿var filteredColumn = 1; // Колонка для фильтрации
+var rows = []; // Все строки таблицы для фильтрации
+
+/**
+ * При загрузке страницы все строки таблицы в массив
+ */
+$(document).ready(function() {
+    rows = $(".table tr:not(:first-child)");
+});
+
+/**
+ * Выбор колонки для фильтра
+ */
+$(".books-filter .dropdown-menu li").click(function () {
+    var item = $(this);
+
+    item.parents(".input-group-btn").find(".filter-title").text(item.text()); // заголовок по какой колонке искать в текст кнопки
+    filteredColumn = +item.attr("id"); // ID колонки для поиска
+
+    // Очистка поля ввода и фокус в него после изменения колонки фильтрации
+    var input = $(".books-filter input");
+    input.val("");
+    input.focus();
+
+    // Отобразить все скрытые строки таблицы
+    rows.each(function () {
+        $(this).show();
+    });
+});
+
+/**
+ * Фильтр
+ */
+$(".books-filter input").on("input", function (event) {
+    var inputText = $(this).val(); // Значение из строки ввода
+
+    rows.each(function () {
+        // Если не совпадает текст в колонке с введенным - скрыть строку
+        if (this.cells[filteredColumn].innerText.toLowerCase().indexOf(inputText.toLowerCase()) < 0) {
+            $(this).hide();
+        }
+        // Отобразить строку
+        else {
+            $(this).show();
+        } 
+    });
+});
+
